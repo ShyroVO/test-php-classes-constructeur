3c354e8993d281f70441dc12599b6c5df21ce8e1
ShyroVO <bosquet.laurie@outlook.com>
2021-02-05 16:25:42 +0100

    dgseqf

--- classes/User.php
+++ classes/User.php
@@ -1,4 +1,14 @@
 <?php
 class User {
+    private string $user;
+    private string $pw;
+
+    public function setOwner(User $user){
+        $this->owner = $user;
+    }
     
+    public function getOwner(): User {
+        return $this->owner;
+    }
+
 }
